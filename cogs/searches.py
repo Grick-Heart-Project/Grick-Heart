f832f37a388f6fde7e081fbb7de8b2d68c59da35
hydrostaticcog <hydro@hydrostaticcog.org>
2021-03-09 17:04:53 -0600

    4.3.1/add magic item lookup tool

--- cogs/searches.py
+++ cogs/searches.py
@@ -1,6 +1,5 @@
 import discord
 import json
-from discord.ext.commands import context
 import requests
 
 from discord.ext import commands
@@ -24,6 +23,11 @@ def weaponAPI(weaponname):
     data = json.loads(request.text)
     return data
 
+def magicitemsAPI(itemname):
+    request = requests.get(f'https://api.open5e.com/magicitems/{itemname}')
+    data = json.loads(request.text)
+    return data
+
 def spellembed(name, source, level, school, time, range, components, duration, classes):
     embed = discord.Embed(title=f'Spell Data for {name}', color=0x239B56)
     embed.add_field(name='Source', value=source, inline='true')
@@ -37,42 +41,19 @@ def spellembed(name, source, level, school, time, range, components, duration, c
     embed.add_field(name='Spell Text', value='Look in message below', inline='true')
     return embed
 
-'''
-def monsterembed(name, size, type, armor_class, hit_points, strength, dexterity, constitution, intelligence, wisdom, charisma, damage_resistances, damages_immunities, condition_immunities, senses, languages, actions, reactions, legendary_actions):
-    if (damage_resistances == ""):
-        damage_resistances = 'None'
-    if (damages_immunities == ""):
-        damages_immunities = 'None'
-    if (condition_immunities == ""):
-        condition_immunities = 'None'
-    if (actions == ""):
-        actions = 'None'
-    if (legendary_actions == ""):
-        legendary_actions = 'None'
-    actions1 = str(actions)
-    embed = discord.Embed(title=f'Monster Data for {name}', color=0x239B56)
-    embed.add_field(name='Size', value=size, inline=False)
-    embed.add_field(name='Type', value=type, inline=False)
-    embed.add_field(name='Armor Class', value=armor_class, inline=True)
-    embed.add_field(name='Hit Points', value=hit_points, inline=True)
-    embed.add_field(name='Strength', value=strength, inline=True)
-    embed.add_field(name='Dexterity', value=dexterity, inline=True)
-    embed.add_field(name='Constitution', value=constitution, inline=True)
-    embed.add_field(name='Intelligence', value=intelligence, inline=True)
-    embed.add_field(name='Wisdom', value=wisdom, inline=True)
-    embed.add_field(name='Charisma', value=charisma, inline=True)
-    embed.add_field(name='Damage Resistances', value=damage_resistances, inline=False)
-    embed.add_field(name='Damage Immunities', value=damages_immunities, inline=False)
-    embed.add_field(name='Condition Immunities', value=condition_immunities, inline=False)
-    embed.add_field(name='Senses', value=senses, inline=False)
-    embed.add_field(name='Languages', value=languages, inline=False)
-    embed.add_field(name='Actions', value=actions1, inline=False)
-    embed.add_field(name='Reactions', value=reactions, inline=False)
-    embed.add_field(name='Legendary Actions', value=legendary_actions, inline=False)
-    return embed
-    #vars = str(name + size + type + str(armor_class) + str(hit_points) + str(strength) + str(dexterity) + str(constitution) + str(intelligence) + str(wisdom) + str(charisma) + damage_resistances +damages_immunities +condition_immunities + senses+ languages +str(actions)+str(reactions)+str(legendary_actions))
-    #return vars
-'''
+    '''
+    def monsterembed(name, size, type, armor_class, hit_points, strength, dexterity, constitution, intelligence, wisdom, charisma, damage_vulnerabilities, damage_immunities, condition_immunities, senses, languages, actions, reactions, legendary_actions):
+        if (damage_vulnerabilities == ''):
+            damage_vulnerabilities = "None"
+        if (damage_immunities == ''):
+            damage_immunities = "None"
+        if (condition_immunities == ''):
+            condition_immunities = "None"
+        if (legendary_actions == ''):
+            legendary_actions = "None"
+        embed = discord.Embed(title=f'Monster Data for {name}', color=0x239B56)
+    '''
+
 
 def weaponembed(name, category, source, cost, damage_dice, damage_type, weight):
     damage = damage_dice +" "+damage_type
@@ -82,6 +63,20 @@ def weaponembed(name, category, source, cost, damage_dice, damage_type, weight):
     embed.add_field(name='Damage', value=damage, inline=True)
     embed.add_field(name='Weight', value=weight, inline=True)
     return embed
+
+def magicitemembed(name, type, rarity, requires_attunement, source):
+    if (requires_attunement == 'requires attunement'):
+        requires_attunement = 'true'
+    else:
+        requires_attunement = 'false'
+    embed = discord.Embed(title=f'Magic Item Data for {name}', color=0x239B56)
+    embed.add_field(name='Source', value=source, inline=True)
+    embed.add_field(name='Type', value=type, inline=True)
+    embed.add_field(name='Rarity', value=rarity, inline=True)
+    embed.add_field(name='Requires Attunement?', value=requires_attunement, inline=True)
+    embed.add_field(name='Description', value='Look in message below', inline=False)
+    return embed
+
 
 class Searches(Cog):
         @commands.command()
@@ -98,19 +93,16 @@ class Searches(Cog):
             except KeyError:
                 await ctx.send(f':octagonal_sign: ERROR: Could not find spell {spellname}')
                 return
-
         '''
         @commands.command()
-        async def monster(self, ctx: MyContext, *, monstername):
+        async def monster(self, ctx: MyContext, monstername):
             monstername1 = '-'.join(monstername.split(' '))
             try:
                 mD = monsterAPI(monstername1)
-                embed = monsterembed(mD['name'], mD['size'], mD['type'], mD['armor_class'], mD['hit_points'], mD['strength'], mD['dexterity'], mD['constitution'], mD['intelligence'], mD['wisdom'], mD['charisma'], mD['damage_resistances'], mD['damage_immunities'], mD['condition_immunities'], mD['senses'], mD['languages'], mD['actions'], mD['reactions'], mD['legendary_actions'])
-                await ctx.send(embed=embed)
+
             except KeyError:
                 await ctx.send(f':octagonal_sign: ERROR: Could not find monster {monstername}')
-                return
-        '''     
+        '''
 
         @commands.command()
         async def weapon(self, ctx: MyContext, *, weaponname):
@@ -132,6 +124,19 @@ class Searches(Cog):
                 except KeyError:
                     await ctx.send(f':octagonal_sign: ERROR: Could not find weapon {weaponname}')
                     return
+        
+        @commands.command(aliases=['mitem'])
+        async def magicitem(self, ctx: MyContext, *, itemname):
+            itemname1 = '-'.join(itemname.split(' '))
+            try:
+                iD = magicitemsAPI(itemname1)
+                embed = magicitemembed(iD['name'], iD['type'], iD['rarity'], iD['requires_attunement'], iD['document__slug'])
+                await ctx.send(embed=embed)
+                itemText = iD['desc']
+                await ctx.send(f'Item Description: {itemText}')
+            except KeyError:
+                await ctx.send(f':octagonal_sign: ERROR: Could not find magic item {itemname}')
+                return
 
 
 setup = Searches.setup
