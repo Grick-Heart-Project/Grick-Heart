f68b352223347621262bc4e56bbadc0d531bda8b
hydrostaticcog <hydro@hydrostaticcog.org>
2021-04-04 13:19:15 -0500

    Update custom_help.py

--- utils/custom_help.py
+++ utils/custom_help.py
@@ -25,7 +25,7 @@ class EmbedHelpCommand(commands.HelpCommand):
 
     bot = commands.Bot(help_command=EmbedHelpCommand())
     """
-    COLOUR = 0x239B56
+    COLOUR = 0xF1C40F
 
     def get_ending_note(self):
         return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
