92cb6a61dbb49247dad7a1ea3580a59b20db113b
hydrostaticcog <hydro@lucidia.us>
2021-04-10 12:20:34 -0500

    4.4.2/adds statcord support

---
+++ cogs/statcord.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+import statcord
+from toml import load
+
+from utils.config import load_config
+from utils.ctx_class import MyContext
+
+config = load_config()
+
+
+class StatcordPost(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.key = config['auth']['statcord']['token']
+        self.api = statcord.Client(self.bot,self.key)
+        self.api.start_loop()
+
+
+    @commands.Cog.listener()
+    async def on_command(self,ctx):
+        self.api.command_run(ctx)
+
+    @commands.command()
+    async def stats(self, ctx: MyContext):
+        await ctx.send(f'Check out {self.bot.user.name}\'s Statcord page at https://statcord.com/bot/778756422275956766')
+
+
+def setup(bot):
+    bot.add_cog(StatcordPost(bot))
